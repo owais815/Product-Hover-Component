beeeb7ba5b5b73fa7bf9e10c037c3c9427309f0e
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2024-07-08 20:56:05 +0500

    final changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let container = document.getElementById("container");
-for (let i = 50; i >= -50; i--) {
+for (let i = 50; i >= -20; i--) {
   let span = document.createElement("span");
   span.style.setProperty("--i", i);
   container.appendChild(span);
